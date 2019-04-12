46010ee8c304a50aa59fa5c35fdf66d383117f42
Sunny Narang <narangsunny65@gmail.com>
2019-04-13 01:43:53 +0530

    Update

---
+++ src/components/CardsSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardSection = (props) => {
+    return (
+        <View style = {styles.containerStyle}>
+        {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+        padding: 10,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative',
+        borderBottomWidth: 1,
+    }
+};
+
+export default CardSection;
